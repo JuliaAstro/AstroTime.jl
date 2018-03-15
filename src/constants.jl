a3a048e92a81e118f740d6c1ccbd25e5a6f6c821
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2018-03-15 22:14:07 +0530

    1

--- src/constants.jl
+++ src/constants.jl
@@ -4,15 +4,7 @@ export MJD, J2000, J1950,
     HOURS_PER_DAY, HOURS_PER_YEAR, HOURS_PER_CENTURY,
     DAYS_PER_YEAR, DAYS_PER_CENTURY,
     YEARS_PER_CENTURY,
-<<<<<<< aeacb6edd2919cee100e9c87a80df84ee2801bb2
-<<<<<<< 56a7a738ef60532ddd337827a009c79f33aa4eeb
     OFFSET_TT_TAI
-=======
-    TTMTAI
->>>>>>> Ported function eraTaitt
-=======
-    OFFSET_TT_TAI
->>>>>>> fixing
 
 const MJD = 2400000.5
 const J2000 = Dates.datetime2julian(DateTime(2000, 1, 1, 12, 0, 0))
@@ -38,19 +30,4 @@ const DAYS_PER_CENTURY     = 365.25 * 100.0
 
 const YEARS_PER_CENTURY    = 100.0
 
-<<<<<<< e7240b131e015741152ce49f3097621a420009c6
-<<<<<<< cdc10b523967f6ca20cf0e65d3a45c8a322678b6
-<<<<<<< 56a7a738ef60532ddd337827a009c79f33aa4eeb
 const OFFSET_TT_TAI = 32.184
-=======
-# ============================= ERFA CONSTANTS =========================
-<<<<<<< aeacb6edd2919cee100e9c87a80df84ee2801bb2
-const TTMTAI = 32.184 # ERFA_TTMTAI
->>>>>>> Ported function eraTaitt
-=======
-=======
->>>>>>> final changes
-=======
->>>>>>> port ut1tai
-const OFFSET_TT_TAI = 32.184
->>>>>>> fixing
