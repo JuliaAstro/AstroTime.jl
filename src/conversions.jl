963174daabd8cf2d56132ce07a048c345d448b12
Ashish Priyadarshi <ashishpriyadarshi1@gmail.com>
2018-03-16 18:17:55 +0530

    Update conversions.jl

--- src/conversions.jl
+++ src/conversions.jl
@@ -5,6 +5,28 @@ export rescale
 using ..LeapSeconds
 
 
+"""
+    tttai(jd1, jd2)
+Transform a two-part Julia date from `TT` to `TAI`.
+# Example
+```jldoctest
+julia> tt  = Epoch{TT}(2.4578265e6, 0.30477440993249416)
+2017-03-14T07:18:20.325 TT 
+julia> AstronomicalTime.Epochs.tttai(tt.jd1, tt.jd2)
+(2.4578265e6, 0.30440190993249416)
+```
+"""
+function tttai(jd1, jd2)
+    dtat = OFFSET_TT_TAI/SECONDS_PER_DAY;
+    if jd1 > jd2
+        jd1 = jd1
+        jd2 -= dtat
+    else
+        jd1 -= dtat
+        jd2 = jd2
+    end
+    jd1, jd2
+end
 function deltatr(ep::Epoch)
     jd1, jd2 = julian1(ep), julian2(ep)
     eraDtdb(jd1, jd2, 0.0, 0.0, 0.0, 0.0)
@@ -71,19 +93,11 @@ function rescale(::Type{UT1Epoch}, ep::TTEpoch)
 end
 
 # TAI <-> TT
-function rescale(::Type{TAIEpoch}, ep::TTEpoch)
-    jd1, jd2 = julian1(ep), julian2(ep)
-    dtat = OFFSET_TT_TAI/SECONDS_PER_DAY;
-    if jd1 > jd2
-        jd1 = jd1
-        jd2 -= dtat
-    else
-        jd1 -= dtat
-        jd2 = jd2
-    end
-    
-    TAIEpoch(jd1, jd2)
-end
+ function rescale(::Type{TAIEpoch}, ep::TTEpoch)
+     jd1, jd2 = julian1(ep), julian2(ep)
+     date, date1 = tttai(jd1, jd2)
+     TAIEpoch(date, date1)
+ end
 
 function rescale(::Type{TTEpoch}, ep::TAIEpoch)
     jd1, jd2 = julian1(ep), julian2(ep)
