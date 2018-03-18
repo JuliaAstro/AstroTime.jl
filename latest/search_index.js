ef74317e46a396982b0a4d4a6ecf9ded4477f1ac
autodocs <autodocs>
2018-03-18 12:10:31 +0000

    build based on bb04903

--- latest/search_index.js
+++ latest/search_index.js
@@ -13,7 +13,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "AstronomicalTime",
     "category": "section",
-    "text": "Astronomical time keeping in Julia[![Build Status Unix][travis-badge]][travis-url] [![Build Status Windows][av-badge]][av-url] [![Coveralls][coveralls-badge]][coveralls-url] [![Codecov][codecov-badge]][codecov-url] [![Docs Stable][docs-badge-stable]][docs-url-stable] [![Docs Latest][docs-badge-latest]][docs-url-latest]"
+    "text": "Astronomical time keeping in Julia"
 },
 
 {
@@ -29,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Quickstart",
     "category": "section",
-    "text": "# Create an Epoch based on the TT (Terrestial Time) scale\ntt = TTEpoch(\"2018-01-01T12:00:00\")\n\n# Transform to UTC (Universal Time Coordinated)\nutc = UTCEpoch(tt)\n\n# Transform to TDB (Barycentric Dynamical Time)\nutc = TDBEpoch(utc)Read the API docs.[travis-badge]: https://travis-ci.org/JuliaAstro/AstronomicalTime.jl.svg?branch=master [travis-url]: https://travis-ci.org/JuliaAstro/AstronomicalTime.jl [av-badge]: https://ci.appveyor.com/api/projects/status/13l2bwswxbl1g8cq?svg=true [av-url]: https://ci.appveyor.com/project/helgee/astronomicaltime-jl [coveralls-badge]: https://coveralls.io/repos/github/JuliaAstro/AstronomicalTime.jl/badge.svg?branch=master [coveralls-url]: https://coveralls.io/github/JuliaAstro/AstronomicalTime.jl?branch=master [codecov-badge]: http://codecov.io/github/JuliaAstro/AstronomicalTime.jl/coverage.svg?branch=master [codecov-url]: http://codecov.io/github/JuliaAstro/AstronomicalTime.jl?branch=master [docs-badge-latest]: https://img.shields.io/badge/docs-latest-blue.svg [docs-url-latest]: https://juliaastro.github.io/AstronomicalTime.jl/latest [docs-badge-stable]: https://img.shields.io/badge/docs-stable-blue.svg [docs-url-stable]: https://juliaastro.github.io/AstronomicalTime.jl/stable"
+    "text": "# Create an Epoch based on the TT (Terrestial Time) scale\ntt = TTEpoch(\"2018-01-01T12:00:00\")\n\n# Transform to UTC (Universal Time Coordinated)\nutc = UTCEpoch(tt)\n\n# Transform to TDB (Barycentric Dynamical Time)\nutc = TDBEpoch(utc)Read the API docs."
 },
 
 {
