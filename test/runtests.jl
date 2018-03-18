045e04af15ee8494b29137eeef9591df0bd810d2
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2018-03-18 21:13:09 +0530

    tdbtt done

--- test/runtests.jl
+++ test/runtests.jl
@@ -172,6 +172,9 @@ AstronomicalTime.update()
         @test AstronomicalTime.Epochs.tttcg(julian2(tt), julian1(tt)) == ERFA.tttcg(julian2(tt), julian1(tt))
         @test AstronomicalTime.Epochs.taiutc(julian1(tai), julian2(tai)) == ERFA.taiutc(julian1(tai), julian2(tai))
         @test AstronomicalTime.Epochs.taiutc(julian2(tai), julian1(tai)) == ERFA.taiutc(julian2(tai), julian1(tai))
+        @test AstronomicalTime.Epochs.dtdb(julian1(tdb), julian2(tdb), 0.0, 0.0, 0.0, 0.0) ≈ ERFA.dtdb(julian1(tdb), julian2(tdb), 0.0, 0.0, 0.0, 0.0)
+@test sum(AstronomicalTime.Epochs.tdbtt(julian1(tdb), julian2(tdb), Δtr(tdb))) ≈ sum(ERFA.tdbtt(julian1(tdb), julian2(tdb), Δtr(tdb)))
+@test sum(AstronomicalTime.Epochs.tdbtt(julian2(tdb), julian1(tdb), Δtr(tdb))) ≈ sum(ERFA.tdbtt(julian2(tdb), julian1(tdb), Δtr(tdb)))
     end
     @testset "Leap Seconds" begin
         @test leapseconds(TTEpoch(1959,1,1)) == 0
