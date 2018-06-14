7a08221f2ff38364538cc6fe2f8299c1021433fe
Prakhar Srivastava <srivastavaprakhar1@gmail.com>
2018-06-14 17:32:21 +0530

    fixes

--- src/conversions.jl
+++ src/conversions.jl
@@ -565,6 +565,7 @@ function cal2jd(iy, im, id)
     jd, jd1
 end
 
+
 """
    utctai(jd1, jd2)
 
@@ -615,41 +616,38 @@ function utctai(jd1, jd2)
     end
     date, date1
 
-function dtf2d(scale, iy, im, id, ihr, imn, sec)
-    jd, w = cal2jd(iy, im, id)
-    jd += w
-    seclim = 60.0
+function dtfield2julian(scale::T, year, month, date, hour, min, sec) where {T <: TimeScale}
 
-    if scale == "UTC"
+    jd = sum(cal2jd(year, month, date))
+    seclim = 60.0
+    adjusted_seconds_per_day = SECONDS_PER_DAY
+    if hour < 0 || hour > 23
+        throw(ArgumentError("The input hour value should be greater than 0"))
+    end
+    if min < 0 || min > 59
+        throw(ArgumentError("The input minute value should be greater than 0"))
+    end
+
+    if scale == UTC
         dat0 = leapseconds(jd)
         dat12 = leapseconds(jd + 0.5)
         dat24 = leapseconds(jd + 1.5)
 
         dleap = dat24 - (2.0 * dat12 - dat0)
         adjusted_seconds_per_day = SECONDS_PER_DAY + dleap
-
-        if ihr == 23 && imn == 59
+        if hour == 23 && min == 59
             seclim += dleap
         end
     end
 
-    if ihr >= 0 && ihr <= 23
-        if imn >= 0 && imn <= 59
-                if sec >= 0
-                    if sec >= seclim
-                        throw(ArgumentError("Time exceeds the maximum seconds in $(iy)-$(im)-$(id)"))
-                    end
-                else
-                    throw(ArgumentError("The input second value should be greater than 0"))
-                end
-            else
-                throw(ArgumentError("The input minute value should be greater than 0"))
-            end
-        else
-            throw(ArgumentError("The input hour value should be greater than 0"))
+    if sec >= 0
+        if sec >= seclim
+            throw(ArgumentError("Time exceeds the maximum seconds in $(year)-$(month)-$(date)"))
         end
-    time  = ( 60.0 * ( 60.0 * ihr + imn )  + sec ) / adjusted_seconds_per_day
-
+    else
+        throw(ArgumentError("The input second value should be greater than 0"))
+    end
+    time  = ( 60.0 * ( 60.0 * hour + min )  + sec ) / adjusted_seconds_per_day
     jd, time
 end
 
