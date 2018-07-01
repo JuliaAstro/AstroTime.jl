8e526b6f826ea58fce1c39db1e993ac4e0962122
Helge Eichhorn <git@helgeeichhorn.de>
2018-07-01 18:56:57 +0200

    Test infinites

--- test/epochs.jl
+++ test/epochs.jl
@@ -2,4 +2,11 @@
     ep = Epoch2{TDB}(100000, 1e-18)
     ep1 = Epoch2{TDB}(ep, 100 * 365.25 * 86400)
     @test ep.offset == ep1.offset
+
+    ep1 = Epoch2{TDB}(ep, Inf)
+    @test ep1.epoch == typemax(Int64)
+    @test ep1.offset == Inf
+    ep1 = Epoch2{TDB}(ep, -Inf)
+    @test ep1.epoch == typemin(Int64)
+    @test ep1.offset == -Inf
 end
