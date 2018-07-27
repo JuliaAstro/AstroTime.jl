724c6484a633e231b10cc154b0ece34e6350cb60
Helge Eichhorn <git@helgeeichhorn.de>
2018-07-27 16:20:41 +0200

    Test simplified conversion

--- src/Epochs2.jl
+++ src/Epochs2.jl
@@ -2,6 +2,7 @@ module Epochs2
 
 using LeapSeconds: offset_tai_utc
 using EarthOrientation: getΔUT1
+using Dates: DateTime, datetime2julian
 
 using ..TimeScales
 
@@ -9,6 +10,10 @@ export Epoch2
 
 const OFFSET_TAI_TT = 32.184
 const SECONDS_PER_DAY = 86400.0
+const J2000_EPOCH = datetime2julian(DateTime(2000, 1, 1, 12, 0, 0)) * SECONDS_PER_DAY
+const REF_EPOCH = datetime2julian(DateTime(1977, 1, 1)) * SECONDS_PER_DAY
+const LG_RATE = 6.969290134e-10
+const LB_RATE = 1.550519768e-8
 
 struct Epoch2{S, T}
     epoch::Int64
@@ -38,10 +43,23 @@ end
 
 Epoch2{S}(ep::Epoch2{S}, Δt) where {S} = Epoch2{S}(ep.epoch, ep.offset, Δt)
 
+function Epoch2{S2}(ep::Epoch2{S1}) where {S1, S2}
+    Δt = offset(S2, ep) - offset(S1, ep)
+    Epoch2{S2}(ep.epoch, ep.offset, Δt)
+end
+
 julian(ep::Epoch2) = (ep.epoch + ep.offset) / SECONDS_PER_DAY
 
 offset(::InternationalAtomicTime, ep) = 0.0
-offset(::TerrestrialTime, ep) = -OFFSET_TAI_TT
+offset(::TerrestrialTime, ep) = OFFSET_TAI_TT
 offset(::CoordinatedUniversalTime, ep) = offset_tai_utc(julian(ep))
+offset(::UniversalTime, ep) = offset(UTC, ep) + getΔUT1(julian(ep))
+offset(::GeocentricCoordinateTime, ep) = offset(TT, ep) + LG_RATE * (ep.epoch - REF_EPOCH + ep.offset)
+offset(::BarycentricCoordinateTime, ep) = offset(TT, ep) + LB_RATE * (ep.epoch - REF_EPOCH + ep.offset)
+function offset(::BarycentricDynamicalTime, ep)
+    dt = (ep.epoch - J2000 + ep.offset) / SECONDS_PER_DAY
+    g = 357.53 + 0.9856003dt
+    offset(TT, ep) + 0.001658sind(g) + 0.000014sind(2g)
+end
 
 end
--- test/epochs.jl
+++ test/epochs.jl
@@ -2,7 +2,6 @@
     ep = Epoch2{TDB}(100000, 1e-18)
     ep1 = Epoch2{TDB}(ep, 100 * 365.25 * 86400)
     @test ep.offset == ep1.offset
-<<<<<<< HEAD
 
     ep1 = Epoch2{TDB}(ep, Inf)
     @test ep1.epoch == typemax(Int64)
@@ -10,7 +9,13 @@
     ep1 = Epoch2{TDB}(ep, -Inf)
     @test ep1.epoch == typemin(Int64)
     @test ep1.offset == -Inf
+
+    tai = Epoch2{TAI}(100000, 0.0)
+    tt = Epoch2{TT}(tai)
+    @test tt.epoch == 100032
+    @test tt.offset ≈ 0.184
+    tai1 = Epoch2{TAI}(tt)
+    @test tai1.epoch == 100000
+    @test tai1.offset ≈ 0.0
 end
-=======
-end
->>>>>>> Add high-accuracy epoch type
+
