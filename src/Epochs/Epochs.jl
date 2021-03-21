59aaa990cb00b4bc3a415b9861a11bb50fc43d71
Helge Eichhorn <git@helgeeichhorn.de>
2021-03-21 14:01:51 +0100

    Cleanup imports and exports in Epochs

--- src/Epochs/Epochs.jl
+++ src/Epochs/Epochs.jl
@@ -1,84 +1,30 @@
 module Epochs
 
 using LeapSeconds: offset_tai_utc
-using ..TimeScales: find_path
 using EarthOrientation: getΔUT1, EOP_DATA, get
 using LeapSeconds: offset_tai_utc, offset_utc_tai
 using MuladdMacro
 
-import Base: -, +, <, ==, isapprox, isless, show
+import Base: (:), -, +, <, ==, isapprox, isless, show
 import Dates
 import Dates: format, parse
 
-import ..AstroDates:
-    Date,
-    DateTime,
-    Time,
-    calendar,
-    day,
-    dayofyear,
-    fractionofday,
-    hour,
-    j2000,
-    julian,
-    julian_twopart,
-    millisecond,
-    minute,
-    month,
-    second,
-    secondinday,
-    year,
-    yearmonthday
+import ..AstroDates: Date, DateTime, Time
+import ..AstroDates: year, day, month, hour, minute, second, millisecond
+import ..AstroDates: calendar, yearmonthday, dayofyear, fractionofday, secondinday
+import ..AstroDates: j2000, julian, julian_twopart
 
-import ..AstroDates: Date, DateTime, Time,
-    year, month, day,
-    hour, minute, second, millisecond,
-    fractionofday, yearmonthday, dayofyear
+using ..TimeScales: find_path
 
-import Base: (:)
-
-
-export Epoch,
-    CCSDS_EPOCH,
-    FIFTIES_EPOCH,
-    FUTURE_INFINITY,
-    GALILEO_EPOCH,
-    GPS_EPOCH,
-    J2000_EPOCH,
-    J2000_TO_JULIAN,
-    J2000_TO_MJD,
-    JULIAN_EPOCH,
-    MODIFIED_JULIAN_EPOCH,
-    PAST_INFINITY,
-    UNIX_EPOCH,
-    DateTime,
-    Date,
-    Time,
-    day,
-    dayofyear,
-    fractionofday,
-    hour,
-    j2000,
-    julian,
-    julian_period,
-    julian_twopart,
-    millisecond,
-    minute,
-    modified_julian,
-    month,
-    now,
-    second,
-    secondinday,
-    timescale,
-    year,
-    yearmonthday,
-    -
-
-export
-    NoOffsetError,
-    NoPathError,
-    getoffset,
-    insideleap
+export CCSDS_EPOCH, FIFTIES_EPOCH, FUTURE_INFINITY, GALILEO_EPOCH, GPS_EPOCH, J2000_EPOCH
+export J2000_TO_JULIAN, J2000_TO_MJD, JULIAN_EPOCH, MODIFIED_JULIAN_EPOCH, PAST_INFINITY
+export UNIX_EPOCH
+export Date, DateTime, Time, Epoch
+export NoOffsetError, NoPathError
+export day, dayofyear, fractionofday, millisecond, minute, hour, month, now, second
+export secondinday, timescale, year, yearmonthday
+export getoffset, insideleap
+export j2000, julian, julian_period, julian_twopart, modified_julian
 
 using ..TimeScales
 using ..AstroDates
