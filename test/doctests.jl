2f84feecde00c97e5b119526087b4e87b99001d9
Abhro R <5664668+abhro@users.noreply.github.com>
2024-05-25 16:07:28 -0400

    Make doctests in test suite version dependent
    Julia 1.6 changes display of 1- and 2-d arrays.
    Don't rely on them for doctests.

--- test/doctests.jl
+++ test/doctests.jl
@@ -3,6 +3,7 @@ using Documenter
 setup = quote
     using AstroTime
 end
-DocMeta.setdocmeta!(AstroTime, :DocTestSetup, setup; recursive = true)
-doctest(AstroTime)
-
+if VERSION >= v"1.6.0"
+    DocMeta.setdocmeta!(AstroTime, :DocTestSetup, setup; recursive=true)
+    doctest(AstroTime)
+end
